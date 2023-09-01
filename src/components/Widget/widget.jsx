63b043638ae631308a75504116ac834db6196856
Zmeimor <81415017+Zmeimor@users.noreply.github.com>
2023-09-01 10:44:32 +0200

    fff

--- src/components/Widget/widget.jsx
+++ src/components/Widget/widget.jsx
@@ -6,8 +6,10 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 	return (
 		<>
 			{options.map((option) => (
-				<button key={shortid.generate()} type="button" name={option} onClick={onLeaveFeedback}>
-					{option}
+			    // 	<button key={shortid.generate()} type="button" name={option} onClick={onLeaveFeedback}>
+			    // 		{option}
+				<button key={option} type="button" name={option} onClick={onLeaveFeedback}>
+				{option}
 				</button>
 			))}
 		</>
