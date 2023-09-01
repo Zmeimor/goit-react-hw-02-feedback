17ae5f31e1e6fc6715a9f3f3f7504d8fcaf43786
Zmeimor <81415017+Zmeimor@users.noreply.github.com>
2023-09-01 12:05:11 +0200

    ппп

--- src/components/Widget/widget.jsx
+++ src/components/Widget/widget.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-  // 	import shortid from 'shortid';
 import PropTypes from 'prop-types';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 	return (
 		<>
 			{options.map((option) => (
-			    // 	<button key={shortid.generate()} type="button" name={option} onClick={onLeaveFeedback}>
-			    // 		{option}
 				<button key={option} type="button" name={option} onClick={onLeaveFeedback}>
 				{option}
 				</button>
